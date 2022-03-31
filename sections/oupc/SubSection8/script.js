34f503ced0cf95cb9b1627b2471e1a72248d1417
Junhand <jun1493907@gmail.com>
2022-03-31 09:02:18 +0900

    #6 Access Sectionの完成

--- sections/oupc/SubSection8/script.js
+++ sections/oupc/SubSection8/script.js
@@ -2,7 +2,7 @@ export default {
   name: 'SubSection8',
   data: () => ({
     mapURL:
-      'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3275.360867873206!2d135.5222789155346!3d34.82201828421804!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6000fb60db96a653%3A0xf584717b6ac7c9ef!2z5aSn6Ziq5aSn5a2m!5e0!3m2!1sja!2sjp!4v1646917496927!5m2!1sja!2sjp',
+      'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2918.635174207908!2d135.45253916554623!3d34.80449781823799!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x803d94bd5051b99b!2z5piO6YGT6aSo!5e0!3m2!1sja!2sjp!4v1648626452181!5m2!1sja!2sjp',
     mapHeight: 350,
   }),
 }
