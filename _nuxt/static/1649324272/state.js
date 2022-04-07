f2c8a5afd879260e9215ab99aa78541b14d6fe96
litchi7777 <litchi7777@gmail.com>
2022-04-07 19:10:38 +0900

    Updates

--- _nuxt/static/1649324272/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Foupc-web\u002F_nuxt\u002Fstatic\u002F1649324272",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Foupc-web\u002F",assetsPath:"\u002Foupc-web\u002F_nuxt\u002F",cdnURL:a}}}}(null));
