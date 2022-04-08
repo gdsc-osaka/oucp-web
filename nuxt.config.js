cf7b8f5380e3556a051623d69352a9bd053eb044
Junhand <30954368+Junhand@users.noreply.github.com>
2022-04-09 08:45:06 +0900

    fix: url image

--- nuxt.config.js
+++ nuxt.config.js
@@ -62,7 +62,7 @@ export default {
       {
         hid: 'og:image',
         property: 'og:image',
-        content: 'https://gdsc-osaka.github.io/oucp-web//ouen.JPG',
+        content: 'https://gdsc-osaka.github.io/oucp-web/ouen.JPG',
       },
 
       {
