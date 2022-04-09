8d1ea7044bce2dc7e1c10bccf605a925877080a4
Junhand <jun1493907@gmail.com>
2022-04-09 16:56:27 +0900

    Updates

---
+++ _nuxt/static/1649490939/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Foucp-web\u002F_nuxt\u002Fstatic\u002F1649490939",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Foucp-web\u002F",assetsPath:"\u002Foucp-web\u002F_nuxt\u002F",cdnURL:a}}}}(null));
